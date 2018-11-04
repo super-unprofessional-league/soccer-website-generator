66e6324c55f88c1f3bcf8dfd19ff4a01a7dd51ff
shrekshao <shrekshao@gmail.com>
2018-11-03 20:48:24 -0700

    season table

--- _plugins/league_generator.rb
+++ _plugins/league_generator.rb
@@ -1,26 +1,50 @@
 module League
-    # class SeasonPage < Jekyll::Page
-    class SeasonPage < Jekyll::PageWithoutAFile
+
+    # class TeamPage < Jekyll::Page
+    #     def initialize(site, base, dir, team)
+    #         @site = site
+    #         @base = base
+    #         @dir = dir
+    #         @name = 'index.html'
+
+    #         self.process(@name)
+    #         self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+    #         self.data['category'] = category
+
+    #         category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+    #         self.data['title'] = "#{category_title_prefix}#{category}"
+    #     end
+    # end
+
+    class SeasonPage < Jekyll::Page
+    # class SeasonPage < Jekyll::PageWithoutAFile
         def initialize(site, base, dir, season)
             @site = site
             @base = base
             @dir = dir
-            @path = dir
             # @name = "#{season[0]}.html"
             @name = "index.html"
-            # @layout = 'default.html'
-            @path = if site.in_theme_dir(base) == base # we're in a theme
-                site.in_theme_dir(base, dir, name)
-              else
-                site.in_source_dir(base, dir, name)
-              end
+            # @layout = 'season.html'
+            # @path = if site.in_theme_dir(base) == base # we're in a theme
+            #     site.in_theme_dir(base, dir, name)
+            #   else
+            #     site.in_source_dir(base, dir, name)
+            #   end
 
-            puts @path
+            # puts @path
+
             self.process(@name)
             
-            # self.data = season[1]['table'][0]['name'].to_s
-            self.data = season[1]
+            
+            # self.data = season[1]
+            puts base
+            self.read_yaml(File.join(base, '_layouts'), 'season.html')
 
+            # self.data = {}
+            self.data['title'] = season[0]
+            self.data['table'] = season[1]['table']
+
+            # puts self.data['table']
             # puts dir
         end
     end
@@ -44,32 +68,7 @@ module League
         end
     end
 
-    # class TeamPage < Page
-    #     def initialize(site, base, dir, team)
-    #         @site = site
-    #         @base = base
-    #         @dir = dir
-    #         @name = 'index.html'
 
-    #         self.process(@name)
-    #         self.read_json(File.join(base, '_layouts'), 'category_index.html')
-    #         self.data['category'] = category
 
-    #         category_title_prefix = site.config['category_title_prefix'] || 'Category: '
-    #         self.data['title'] = "#{category_title_prefix}#{category}"
-    #     end
-    # end
 
-    # class TeamPageGenerator < Generator
-    #     safe true
-
-    #     def generate(site)
-    #         if site.layouts.key? 'category_index'
-    #             dir = site.config['team_dir'] || 'team'
-    #             site.categories.each_key do |category|
-    #             site.pages << TeamPage.new(site, site.source, File.join(dir, category), category)
-    #             end
-    #         end
-    #     end
-    # end
 end
