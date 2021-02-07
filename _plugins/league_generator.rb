d3dee4ede435ab3c8d32cf440c712f2a04445823
shrekshao <shrekshao@gmail.com>
2021-02-07 15:12:38 -0800

    calculate table auto

--- _plugins/league_generator.rb
+++ _plugins/league_generator.rb
@@ -36,10 +36,64 @@ module League
         end
     end
 
+    def self.calculate_table (team_hash, games_pair)
+        # Iterate games to calculate data
+
+        # clear data 
+        for team in team_hash
+            # if team['table'] == nil
+            #     team['table'] = Hash.new
+            # end
+            team[1]['table'] = {
+                'games_played' => 0,
+                'wins' => 0,
+                'draws' => 0,
+                'loses' => 0,
+                'goals_for' => 0,
+                'goals_against' => 0,
+                'goals_diff' => 0,
+                'points' => 0
+            }
+        end
+
+        for p in games_pair
+            key = p[0]
+            game = p[1]
+
+            t0 = team_hash[game['home']['key']]['table']
+            t0['games_played'] += 1;
+            t0['goals_for'] += game['home']['score'];
+            t0['goals_against'] += game['away']['score'];
+
+            t1 = team_hash[game['away']['key']]['table']
+            t1['games_played'] += 1;
+            t1['goals_for'] += game['away']['score'];
+            t1['goals_against'] += game['home']['score'];
+            
+            if game['home']['score'] > game['away']['score']
+                t0['wins'] += 1;
+                t1['loses'] += 1;
+            elsif game['home']['score'] < game['away']['score']
+                t1['wins'] += 1;
+                t0['loses'] += 1;
+            else
+                t0['draws'] += 1;
+                t1['draws'] += 1;
+            end
+        end
+
+        # post process
+        for team in team_hash
+            t = team[1]['table']
+            t['goals_diff'] = t['goals_for'] - t['goals_against']
+            t['points'] = t['wins'] * 3 + t['draws']
+        end
+    end
+
     # default SeasonPage (League table)
     class SeasonPage < Jekyll::Page
     # class SeasonPage < Jekyll::PageWithoutAFile
-        def initialize(site, base, dir, season, team_array, games_pair)
+        def initialize(site, base, dir, season, team_hash, games_pair)
             @site = site
             @base = base
             @dir = dir
@@ -64,10 +118,14 @@ module League
             # self.data = {}
             self.data["title"] = season
 
+            League.calculate_table(team_hash, games_pair)
+            # Object.send(:calculate_table, team_hash, games_pair)
+            team_tables = team_hash.map{ |key, value| value }
 
             # team_array = (season[1]['teams'].to_a).map{|key, team| team}
 
-            sorted = (team_array.sort_by { |team| [ -team['table']['points'], -team['table']['goals_for'] + team['table']['goals_against'], -team['table']['goals_for'] ] })
+            sorted = (team_tables.sort_by { |team| [ -team['table']['points'], -team['table']['goals_diff'], -team['table']['goals_for'], team['table']['goals_against'], team['table']['games_played'] ] })
+            # sorted = (team_array.sort_by { |team| [ -team['table']['points'], -team['table']['goals_for'] + team['table']['goals_against'], -team['table']['goals_for'] ] })
             # puts sorted
 
             self.data['table'] = sorted
@@ -136,6 +194,12 @@ module League
 
             ##############################################
 
+            # Iterate games to calculate data
+            group_games_pair = games_pair.select{|key, game| game['type'].include? 'group'}
+            League.calculate_table(team_hash, group_games_pair)
+
+            ##############################################
+
             # group stage
 
             groups = config['group_stage']
@@ -146,7 +210,8 @@ module League
             groups.each do |group_key, team_keys|
                 # simple not points version first
                 team_array = team_keys.map{|key| team_hash[key]}
-                group_tables[group_key] = team_array
+                sorted = (team_array.sort_by { |team| [ -team['table']['points'], -team['table']['goals_diff'], -team['table']['goals_for'], team['table']['goals_against'], team['table']['games_played'] ] })
+                group_tables[group_key] = sorted
             end
 
             self.data['group_tables'] = group_tables
@@ -166,21 +231,22 @@ module League
             site.data['seasons'].each do |season|
                 # convert nilClass to array
 
-                team_array = (season[1]['teams'].to_a).map{|key, team| team}
+                team_hash = season[1]['teams']
+                # team_array = (team_hash.to_a).map{|key, team| team}
 
-                team_hash = Hash.new
+                
 
                 games_pair = season[1]['games'].to_a
 
                 # puts games_pair
 
-                # site['team_pages'] = []
-                # team pages
-                team_array.each do |team|
-                    team_hash[team['key']] = team
-                    # TODO: optimize: parse games_pair once, built team2games map
-                    site.pages << TeamPage.new(site, site.source, File.join('seasons', season[0], team['key']), team, games_pair)
+                team_hash.each do |key, team|
+                    site.pages << TeamPage.new(site, site.source, File.join('seasons', season[0], key), team, games_pair)
                 end
+                # team_array.each do |team|
+                #     # TODO: optimize??: parse games_pair once, built team2games map (more mem)
+                #     site.pages << TeamPage.new(site, site.source, File.join('seasons', season[0], team['key']), team, games_pair)
+                # end
 
                 games_hash = Hash.new
 
@@ -206,7 +272,7 @@ module League
                 config = season[1]['config']
                 # puts config
                 if config == nil or config['type'] == 'league'
-                    site.pages << SeasonPage.new(site, site.source, File.join('seasons', season[0]), season[0], team_array, games_pair)
+                    site.pages << SeasonPage.new(site, site.source, File.join('seasons', season[0]), season[0], team_hash, games_pair)
                 elsif config['type'] == 'group + knockout'
                     site.pages << GroupAndKnockOutSeasonPage.new(site, site.source, File.join('seasons', season[0]), season[0], team_hash, games_hash, games_pair, config)
                 end
@@ -219,62 +285,5 @@ module League
             end
         end
     end
-
-    # class SeasonPageGenerator < Jekyll::Generator
-    #     safe true
-
-    #     def generate(site)
-    #         # puts site.pages
-    #         site.data['seasons'].each do |season|
-    #             # convert nilClass to array
-    #             team_array = (season[1]['teams'].to_a).map{|key, team| team}
-
-    #             team_hash = Hash.new
-
-    #             games_pair = season[1]['games'].to_a
-
-    #             # site['team_pages'] = []
-    #             # team pages
-    #             team_array.each do |team|
-    #                 team_hash[team['key']] = team
-    #                 # TODO: optimize: parse games_pair once, built team2games map
-    #                 site.pages << TeamPage.new(site, site.source, File.join('seasons', season[0], team['key']), team, games_pair)
-    #             end
-
-    #             # game pages
-    #             # game_hash = Hash.new{ (season[1]['games'].to_a).map{|key, game| game_hash[key] = game} }
-
-                
-
-    #             # games_hash = Hash.new
-    #             # games_pair.map{|key, game| games_hash[key] = game}
-                
-                
-    #             # puts game_array
-
-    #             games_pair.each do |key, game|
-    #                 # games_hash[key] = game
-
-    #                 home_team = team_hash[game['home']['key']]
-    #                 away_team = team_hash[game['away']['key']]
-
-    #                 game['home']['display_name'] = home_team['display_name']
-    #                 game['home']['logo'] = home_team['logo']
-    #                 game['away']['display_name'] = away_team['display_name']
-    #                 game['away']['logo'] = away_team['logo']
-    #                 # puts home_team
-    #                 site.pages << GamePage.new(site, site.source, File.join('seasons', season[0], 'games', key), game)
-    #             end
-
-    #             # puts games_hash
-    #             # puts games_pair
-    #             # season table page
-    #             site.pages << SeasonPage.new(site, site.source, File.join('seasons', season[0]), season[0], team_array, games_pair)
-    #         end
-    #     end
-    # end
-
-
-
 
 end
